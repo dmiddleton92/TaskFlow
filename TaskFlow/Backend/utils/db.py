9324f680446402384f36d13f8b978a4b80edfa3c
dmiddleton92 <dm.middleton92@gmail.com>
2025-02-19 19:45:17 -0700

    finished database and client

--- TaskFlow/Backend/utils/db.py
+++ TaskFlow/Backend/utils/db.py
@@ -1,2 +1,7 @@
 from pymongo import MongoClient
 from config import MONGO_URI, DB_NAME
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+users_collection = db['users']
+tasks_collection = db['tasks']
